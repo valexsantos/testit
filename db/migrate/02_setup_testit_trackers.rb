25c093a6014e4c3d8f2e8085f19e8ba23c42bdac
Vasco Santos <valexsantos@gmail.com>
2016-03-30 17:12:41 +0100

    update

--- db/migrate/02_setup_testit_trackers.rb
+++ db/migrate/02_setup_testit_trackers.rb
@@ -1,5 +1,26 @@
 class SetupTestitTrackers < ActiveRecord::Migration
 
+
+    def create_issue_status(opts={})
+        stat = IssueStatus.where(opts).first
+        if !stat
+            pos = IssueStatus.all.size
+            stat = IssueStatus.new(opts)
+            stat.position = pos + 1
+            stat.save
+        end
+        stat
+    end
+    def create_tracker(opts={})
+        stat = Tracker.where(opts).first
+        if !stat
+            pos = Tracker.all.size
+            stat = Tracker.new(opts)
+            stat.position = pos +1
+            stat.save
+        end
+        stat
+    end
     def self.up
         # TestCase
         # TestSuite
@@ -7,81 +28,73 @@ class SetupTestitTrackers < ActiveRecord::Migration
         #   status
         #     Ready
         #     Obsoleted
-        new_status=IssueStatus.where(
+        new_status=create_issue_status(
             :name => "New",
             :is_closed => false
-        ).first_or_create
-        in_progress_status = IssueStatus.where(
+        )
+        in_progress_status = create_issue_status(
             :name => "In Progress",
             :is_closed => false
-        ).first_or_create
-        ready_status=IssueStatus.where(
+        )
+        ready_status = create_issue_status(
             :name => "Ready",
             :is_closed => false
-        ).first_or_create
-        obsoleted_status = IssueStatus.where(
+        )
+        obsoleted_status = create_issue_status(
             :name => "Obsoleted",
             :is_closed => false
-        ).first_or_create
-        rejected_status = IssueStatus.where(
+        )
+        rejected_status = create_issue_status(
             :name => "Rejected",
             :is_closed => true
-        ).first_or_create
-        closed_status = IssueStatus.where(
+        )
+        closed_status = create_issue_status(
             :name => "Closed",
             :is_closed => true
-        ).first_or_create
+        )
         # TestExecution
         #   status:
         #     Passed
         #     Failed
         #     Blocked
         #     Not available
-        passed_status = IssueStatus.where(
+        passed_status = create_issue_status(
             :name => "Passed",
             :is_closed => false
-        ).first_or_create
-        failed_status = IssueStatus.where(
+        )
+        failed_status = create_issue_status(
             :name => "Failed",
             :is_closed => false
-        ).first_or_create
-        blocked_status = IssueStatus.where(
+        )
+        blocked_status = create_issue_status(
             :name => "Blocked",
             :is_closed => false
-        ).first_or_create
-        na_status = IssueStatus.where(
+        )
+        na_status = create_issue_status(
             :name => "Not available",
             :is_closed => false
-        ).first_or_create
+        )
         # 
-        tracker_position = Tracker.all.size + 1
         #
-        testcase_tracker =  Tracker.where(
+        testcase_tracker = create_tracker(
             :name => 'Test case',
             :default_status => new_status
-        ).first_or_create
-        testcase_tracker.position = tracker_position
-        tracker_position = tracker_position + 1
+        )
         #
-        testsuite_tracker =  Tracker.where(
+        testsuite_tracker = create_tracker(
             :name => 'Test suite',
             :default_status => new_status
-        ).first_or_create
-        testsuite_tracker.position = tracker_position
-        tracker_position = tracker_position + 1
+        )
         #
-        testplan_tracker =  Tracker.where(
+        testplan_tracker = create_tracker(
             :name => 'Test plan',
             :default_status => new_status
-        ).first_or_create
-        testplan_tracker.position = tracker_position
-        tracker_position = tracker_position + 1
+        )
         #
-        testrun_tracker =  Tracker.where(
+        testrun_tracker = create_tracker(
             :name => 'Test run',
             :default_status => na_status
-        ).first_or_create
-        testrun_tracker.position = tracker_position
+        )
         # workflow
         Role.all.each { |role|
             WorkflowTransition.create!(:tracker => testcase_tracker, :role => role , :old_status => new_status, :new_status => in_progress_status)
