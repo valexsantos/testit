b3149f844317b6be06270cc381d6cb283db0a8cc
Vasco Santos <valexsantos@gmail.com>
2016-05-11 14:46:53 +0100

    Fix oracle issues

--- init.rb
+++ init.rb
@@ -16,7 +16,7 @@ Redmine::Plugin.register :testit do
     name 'TestIt'
     author 'Vasco Santos'
     description 'Test management tool'
-    version '0.2.10'
+    version '0.2.11'
     url 'https://github.com/valexsantos/testit'
     author_url 'https://github.com/valexsantos'
 
